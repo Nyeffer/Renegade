6a5c3a582cbce610d355b594d5df6bbf355660e6
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-15 17:58:07 -0500

    Can Transition to 1st and 3rd person perspective

---
+++ Assets/Scripts/CameraChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+	public bool isFirstPerson;
+	public Transform firstPerson;
+	public Transform thirdPerson;
+
+	void Start() {
+		isFirstPerson = true;
+	}
+
+	void Update() {
+		if(isFirstPerson) {
+			gameObject.transform.position = firstPerson.transform.position;
+			gameObject.transform.rotation = firstPerson.transform.rotation;
+		} else {
+			gameObject.transform.position = thirdPerson.transform.position;
+			gameObject.transform.rotation = thirdPerson.transform.rotation;
+		}
+
+		if(Input.GetKeyDown("space")) {
+			if(isFirstPerson) {
+				isFirstPerson = false;
+			} else {
+				isFirstPerson = true;
+			}
+		}
+	}
+
+
+}
