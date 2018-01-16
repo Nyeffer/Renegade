0b69fcafa35706579253b5db84388db3310c9dab
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-16 18:41:31 -0500

    Character Selection

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour {
+
+	public GameObject CharacterSelection;
+
+	public void StartToLevelSelect() {
+		SceneManager.LoadScene("CharacterSelection", LoadSceneMode.Single);
+	}
+
+	public void RotateCharacterSelectionRight() {
+		CharacterSelection.transform.Rotate(new Vector3(0, 90, 0));
+	}
+
+	public void RotateCharacterSelectionLeft() {
+		CharacterSelection.transform.Rotate(new Vector3(0, -90, 0));
+	}
+}
