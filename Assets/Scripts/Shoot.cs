705e23714231920e8edeba1edd5cdfee65942c9e
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-15 20:21:33 -0500

    Shooting and ViewChange

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform nuzzle;
+
+	public float fireRate;
+	public float lastFired;
+
+	void Start() {
+		fireRate = 10.0f;
+		lastFired = 0f;
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Mouse0)) {
+			if(Time.time - lastFired > 1/fireRate) {
+				lastFired = Time.time;
+				var shoot = (GameObject)Instantiate(bullet, nuzzle.transform.position, nuzzle.transform.rotation);
+				shoot.GetComponent<Rigidbody>().velocity = shoot.transform.forward * 6;
+				Destroy(shoot, 3.0f);
+			}
+		}
+	}
+}
