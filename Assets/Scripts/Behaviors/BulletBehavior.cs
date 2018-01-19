13bae5a123ad176d403c4832f6a3835b904c71a5
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-19 17:54:05 -0500

    fix bullets and made TriShot

--- Assets/Scripts/Behaviors/BulletBehavior.cs
+++ Assets/Scripts/Behaviors/BulletBehavior.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour {
+    private Rigidbody rigidbody;
+    public Vector3 velocity;
+    public float moveSpeed = 3.0f;
 
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+        velocity = rigidbody.velocity;
+    }
+    void Update() {
+        velocity = velocity + this.transform.right * moveSpeed;
+        rigidbody.velocity = velocity;
+        Destroy(this.gameObject, 0.5f);
+    }
 }
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -19,9 +19,7 @@ public class Shoot : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Mouse0)) {
 			if(Time.time - lastFired > 1/fireRate) {
 				lastFired = Time.time;
-				var shoot = (GameObject)Instantiate(bullet, nuzzle.transform.position, nuzzle.transform.rotation);
-				shoot.GetComponent<Rigidbody>().velocity = shoot.transform.forward * 6;
-				Destroy(shoot, 3.0f);
+				Instantiate(bullet, nuzzle.transform.position, nuzzle.transform.rotation);
 			}
 		}
 	}
