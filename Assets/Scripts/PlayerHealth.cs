65692215a213de539be10bdbcaef29c94fc25453
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-01-24 17:13:37 -0500

    Functioning DealDamage Done

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int maxHealth = 100;
+	public int curHealth;
+	public Slider hpBar;
+
+	bool isDead;
+	bool isDamaged;
+
+	void Awake() {
+		hpBar.maxValue = maxHealth;
+		curHealth = maxHealth;
+		TakeDamage(0);
+	}
+
+	void Update() {
+		TakeDamage(1);
+		if(isDamaged) {
+			// Do Something
+		} else {
+			// Do Something else
+		} 
+		isDamaged = false;
+		Debug.Log(curHealth);
+	}
+
+	public void TakeDamage(int damageDealt) {
+		isDamaged = true;
+		curHealth -= damageDealt;
+		hpBar.value = curHealth;
+
+		if(curHealth <= 0 && !isDead) {
+			Death();
+		}
+	}
+
+	void Death() {
+		isDead = true;
+	}
+
+
+}
