8e9f84c4677d7f5c4b5cca1e628890db158835fe
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-04-16 21:37:57 -0400

    add Time Trial

---
+++ Assets/Scripts/Behaviors/ActiveandDeactivate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveandDeactivate : MonoBehaviour {
+
+	public GameObject otherButton;
+	public GameObject thisButton;
+
+	public void Switch() {
+		otherButton.SetActive(true);
+		thisButton.SetActive(false);
+	}
+}
